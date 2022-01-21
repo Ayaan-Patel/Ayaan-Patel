d6d6a89f3ff4beb0c069705c87dfd663f26281e2
Ayaan-Patel <91730139+Ayaan-Patel@users.noreply.github.com>
2022-01-21 15:22:09 +0530

    Delete 1) height.py

--- 1) height.py
+++
@@ -1,3 +0,0 @@
-# This program asks the user for a height in meters, then returns it in millimeters rounded down to the nearest whole mm)
-height = float(input("Enter your height in meters: "))
-print("You are", int(height * 1000),"mm tall")
