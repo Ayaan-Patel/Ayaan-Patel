cba131f2e976bbdee4ce2a6f638a6845d386fcca
Ayaan-Patel <91730139+Ayaan-Patel@users.noreply.github.com>
2022-01-21 15:28:40 +0530

    Update 2) encryption.py

--- 2) encryption.py
+++ 2) encryption.py
@@ -1,12 +1,5 @@
-# To use, enter a word, choose a number, and select to encrypt. It will give a cipher.
-# Then re-run the program, enter the cipher, choose your previously selected key, and select decrypt.
-
-# This program ask the user to input a message that needs to be encrypted. It then asks for a key of choice between 1 - 26 for encryption, and the same key number for decryption.
-
-# The program defines the function encrypt with message and key inputs, using string methods to encrypt the string as well as adding the key number to each letter of the string.
-
-# The program defines the function decrypt by using the cipher (encrypted string) and key number. All the string methods are undone, and the key number is subtracted from each letter to return the original message.
-
+# adding the key number to each letter of the string.
+# key number is subtracted from each letter to return original message.
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LETTERS = LETTERS.lower()
